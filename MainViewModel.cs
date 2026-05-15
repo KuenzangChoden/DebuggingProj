cff2c46008addd3ad70a79ac2c8681ba36bc7987
Kuenzang Choden <kuenzang.choden@developertools.com>
2026-05-15 16:40:06 +0600

    Fix: working prototy

---
+++ MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DebuggingProj
+{
+    public class MainViewModel
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private string _name = "";
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                _name = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Name)));
+            }
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -19,6 +19,7 @@ namespace DebuggingProj
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new MainViewModel();
         }
     }
 }
